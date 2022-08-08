ec04f0d855b7036a3731d9865a5b2e2ccc72a9e6
Deepak Kumar <deepakkumar100vvr@gmail.com>
2022-08-08 15:27:24 +0530

    crpto-tracker web-app

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnE46eDZH5bLTd4Vt9KQ-_jSCsU-E2xUQ",
+  authDomain: "kryptox-cad3d.firebaseapp.com",
+  projectId: "kryptox-cad3d",
+  storageBucket: "kryptox-cad3d.appspot.com",
+  messagingSenderId: "738305158307",
+  appId: "1:738305158307:web:56d1ee03ce7d1dca15e316",
+  measurementId: "G-K744QKCG4L"
+};
+
+export default firebaseConfig;
